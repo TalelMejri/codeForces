3b00a390f67997f16290a020ce4b0210249dc03a
TalelMejri <talelmejri8@gmail.com>
2024-06-08 13:03:32 +0100

    Word Capitalization

--- Helpful Maths/main.cpp
+++ Helpful Maths/main.cpp
@@ -1,9 +1,34 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#include <string>
+#include <sstream>
 using namespace std;
+
+string int_to_string(int num) {
+    stringstream ss;
+    ss << num;
+    return ss.str();
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+   string sum;
+   cin>>sum;
+
+   vector<int> arr;
+   for(int i=0;i<sum.size();i+=2){
+        arr.push_back(sum[i] - '0');
+   }
+
+   sort(arr.begin(),arr.end());
+
+   string res;
+   for(int i=0;i<arr.size();i++){
+        res+=int_to_string(arr[i]);
+        if (i < arr.size() - 1) {
+            res += '+';
+        }
+   }
+
+    cout<<res<<endl;
     return 0;
 }
---
+++ Word Capitalization/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string ch;
+  cin>>ch;
+
+  ch[0]=toupper(ch[0]);
+  cout<<ch;
+}
