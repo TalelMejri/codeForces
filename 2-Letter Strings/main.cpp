9c51d25f507658b60abdf3fe8e002059a09b8266
TalelMejri <talelmejri8@gmail.com>
2024-06-21 22:30:26 +0100

    fix problem

--- 2-Letter Strings/main.cpp
+++ 2-Letter Strings/main.cpp
@@ -7,17 +7,22 @@ int main()
   int t;
   cin>>t;
   while(t--){
-    long long n;
+    int n;
     cin>>n;
     string ch;
     vector<string> arr(n);
     for(int i=0;i<n;i++){
-        cin >> ch;
-        arr.push_back(ch);
+        cin >> arr[i];
     }
-
-      for(int i=0;i<arr.size();i++){
-        cout<<arr[i]<<endl;
+    int nb=0;
+    for(int i=0;i<arr.size();i++){
+          string test=arr[i];
+          for(int j=i+1;j<arr.size();j++){
+                if( (test[0]==arr[j][0] && test[1]!=arr[j][1]) ||  (test[1]==arr[j][1] && test[0]!=arr[j][0])){
+                    nb++;
+                }
+        }
     }
+    cout<<nb<<endl;
   }
 }
