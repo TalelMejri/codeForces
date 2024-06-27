4cecf17186ac5817c743393b61ecf450f1529be6
TalelMejri <talelmejri8@gmail.com>
2024-06-27 17:42:56 +0100

    fix problem

--- Substring and Subsequence/main.cpp
+++ Substring and Subsequence/main.cpp
@@ -2,41 +2,24 @@
 
 using namespace std;
 
-int testValid(char c,string ch){
-    int trouve=-1;
-    for(int i=0;i<ch.size();i++){
-        if(c==ch[i]){
-            trouve=i;
-        }
-    }
-    return trouve;
-}
-
 
 int main()
 {
      int t;
      cin>>t;
      while(t--){
-        string ch1,ch2;
-        cin>>ch1>>ch2;
-        int l=0;
-        int test=ch1.size()>ch2.size() ?  1 : 0;
-        if(test){
-                for(int j=0;j<ch2.size();j++){
-                    if(testValid(ch2[j],ch1)==-1){
-                        l++;
-                    }
-                }
-            cout<<ch1.size()+l<<endl;
-        }else{
-                for(int j=0;j<ch1.size();j++){
-                    if(testValid(ch1[j],ch2)==-1){
-                            l++;
-                    }
-                }
-
-            cout<<ch2.size()+l<<endl;
+       string a,b;
+        cin>>a>>b;
+        int n=a.size(),m=b.size();
+        int lon=0;
+        for(int x=0;x<m;x++){
+        int j=x;
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==b[j]) c++,j++;
         }
+        lon=max(lon,c);
+    }
+        cout<<n+(m-lon)<<"\n";
      }
 }
