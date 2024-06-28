61edf0f20d206aababe542104f8017f97cc23d33
TalelMejri <talelmejri8@gmail.com>
2024-06-28 21:56:18 +0100

    add problem

---
+++ Fireworks/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        long long a,b,m;
+        cin >> a >> b >> m;
+        cout << m/a+m/b+2<<endl;
+    }
+    return 0;
+}
